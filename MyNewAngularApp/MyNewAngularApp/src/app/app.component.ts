e014a746e13641ce1575021f436fec69e11ecc28
sunaynaBp <sunayna_gulab.patil@bp.com>
2019-09-26 09:15:21 +0000

    new checking

--- MyNewAngularApp/MyNewAngularApp/src/app/app.component.ts
+++ MyNewAngularApp/MyNewAngularApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyNewAngularApp';
+  title = 'MyNewAngularApp checking';
 }
